9dd2beb7e51a26b7a43b7c4c7a001e9fe9824e1e
devansh07mehta <devansh1622mht@gmail.com>
2023-01-03 22:59:03 +0530

    Auth api & food Api Completed

--- server/index.js
+++ server/index.js
@@ -4,6 +4,8 @@ import dotenv from "dotenv";
 // Database Connection
 import ConnectDB from "./database/connection";
 
+import Auth from "./api/auth";
+
 dotenv.config();
 
 const zomato = express();
@@ -16,6 +18,9 @@ zomato.get("/", (req, res) => {
     });
 });
 
+//  /auth/signup
+zomato.use("/auth", Auth);
+
 const PORT = 4000
 
 zomato.listen(PORT, () => {
@@ -27,6 +32,6 @@ zomato.listen(PORT, () => {
             console.log("Server is running, but the database connection failed");
             console.log(err);
         })
-    
+
     // console.log("Server is up and running successfully!!");
 });
